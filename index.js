aa03989cc54983e452630b8a0474e797b5b36b51
feuvpi <fredvpg@gmail.com>
2022-11-21 23:57:42 -0300

    creating methods.

--- index.js
+++ index.js
@@ -9,7 +9,77 @@ var string = `{"data":[{"p":7296.89,"s":"BINANCE:BTCUSDT","t":1575526691134,"v":
 
 
 
-exports.CutStr = function() {
+class cutStr {
+    
+    constructor(inputString, lchar, rchar, t1, t2,) {
+        // -- input & output
+        this.inputString = inputString;
+        this.outputString = "";
+        // -- boundary chars
+        this.charLeft = lchar;
+        this.charRight = rchar;
+        console.log(this.charRight)
+        // -- boundary ocurrences 
+        this.charRightOccurrence = t1;
+        this.charLeftOccurrence = t2;
+        // -- contain check
+        this.containLeft = this.contain(this.charLeft);
+        console.log(this.containLeft)
+        this.containRight = this.contain(this.charRight);
+        console.log(this.containRight)
+        // -- boundary index
+        if(this.containLeft){
+            this.indexLeft = this.findIndex(this.charLeft, this.charLeftOccurrence);
+        }
+        if(this.containRight){
+            this.indexRight = this.findIndex(this.charRight), this.charRightOccurrence;
+        }
+
+        // -- output
+        if((typeof(this.indexLeft) == Number) && (typeof(this.indexRight) == Number)){
+            this.outputString = this.input.Substring(this.indexLeft+1, this.indexRight);
+        }
+    }
+
+
+
+    // -- methods ------
+    contain = (char) => {    
+        for (var i = 0; i < this.inputString.length; i++)
+        {
+            let getChar = this.inputString.charAt(i)
+            if(char === getChar){
+                console.log(char)
+                console.log(this.inputString.charAt(i))
+                return true
+            }
+            
+        }
+        return false;
+    }
+    
+    findIndex = (char, count) => {
+        let index = -1;
+        let occurrences = 0;
+        
+        for(const c of this.inputString){
+            index++
+            if(c == char){
+                occurrences++
+            }
+            if(count == occurrences){
+                return(Number)(index);
+            }
+        }
+        return(`There is only ${occurrences} occurrences of ${char} at the input provided.`)
+    
+    }
+
+}
+
+
+
+exports.CutStr = function(string, lchar, rchar, t1, t2,) {
 
     // -- construtores
 
@@ -20,48 +90,50 @@ exports.CutStr = function() {
     //     this.end = indexend;
     // }
 
-    constructor(string, lchar, rchar, t1, t2,) {
-        this.input = inputString;
-        this.output = "";
-        this.startCharacter = lchar;
-        this.endCharacter = rchar;
-        this.startCount = t1;
-        this.endCount = t2;
-    }
+    const cutstr = new cutStr(string, lchar, rchar, t1, t2)
+
+    // constructor(string, lchar, rchar, t1, t2,) {
+    //     this.input = inputString;
+    //     this.output = "";
+    //     this.charLeft = lchar;
+    //     this.charRight = rchar;
+    //     this.charRightOccurrence = t1;
+    //     this.charLeftOccurrence = t2;
+    //     this.indexLeft = null;
+    //     this.indexRight = null;
+    // }
+
+ 
 
     // -- check if string has provided chars
-    const checkChar1 = contain(this.input, this.startCharacter);
-    const checkChar2 = contain(this.input, this.endCharacter);
-
-    if(checkChar1 == false || checkChar2 == false){
-        return (`ERRO. Check the string provided and boundary characters, ${this.startCharacter} or ${this.endCharacter} not found.`)
-    }
-    
-    
-    
-    
-    
+    if(!cutstr.containLeft || !cutstr.containRight) return (`ERROE. Check the string provided and boundary characters, ${this.charLeft} or ${this.charRight} not found.`);
 
 
-    
+    // const checkChar1 = contain(this.input, this.charLeft);
+    // const checkChar2 = contain(this.input, this.charRight);
 
+    // if(checkChar1 == false || checkChar2 == false){
+        
+    // }
+    
+    
     // -- find index of lchar
-        // -- iterate over strings
-        // -- if find lchar, count++
-        // -- if count == t1. index = i
-    
-    // -- repeat for rchar
+    // indexLeft = findIndex(this.input, this.charLeft, this.charLeftOccurrence);
+    // indexRight = findIndex(this.input, this.charRight, this.charRightOccurrence);
+    if(typeof(cutstr.indexLeft) != Number || typeof(cutstr.indexRight) != Number){
+        return(`There was a problem with the number of occurrences provided. Check the number of occurrences of each boundary identifier.`)
+    }
 
-    // -- use substring or split to cut string and place it in output
+    // // -- use substring or split to cut string and place it in output
+    // cutstr.output = this.input.Substring(this.indexLeft+1, this.indexRight)
 
     // -- return output
-
-
-}
-
-module.exports = cutstr(string) {
+    return(this.output)
 
 }
+
+
+
 
 
 // -- methods
@@ -75,3 +147,24 @@ const contain = (str, char) => {
         return false;
     }
 }
+
+const findIndex = (str, char, count) => {
+    let index = -1;
+    let occurrences = 0;
+    
+    for(const c of str){
+        index++
+        if(c == char){
+            occurrences++
+        }
+        if(count == occurrences){
+            return(int)(index);
+        }
+    }
+    return(`There is only ${occurrences} occurrences of ${char} at the input provided.`)
+
+}
+
+
+var test = this.CutStr(string, ":", ",", 2, 1)
+console.log(test)
